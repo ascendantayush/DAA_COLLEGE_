1f5409cbb3b1d3cf749b7a8ca7e3fba36e3e334b
Ayush Raj <161280074+ascendantayush@users.noreply.github.com>
2025-03-10 02:18:37 +0530

    Update KARATSUBA.cpp
    new karastuba algorithm

--- KARATSUBA.cpp
+++ KARATSUBA.cpp
@@ -1,45 +1,43 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-
-long long karatsuba(long long x, long long y){
+long long karatsuba(long long x,long long y){
     if(x < 10 || y < 10){
-        return x * y;
+        return x*y;
     }
     
     int size = 0;
-    long long temp =  x > y ? x : y;
-    while(temp != 0){
+    int temp = x > y ? x : y;
+    while(temp >0){
         temp = temp/10;
         size++;
     }
     
     int half_size = size / 2;
     long long power = 1;
-    for(int i= 0 ; i < half_size ; i++){
+    for(int i= 0 ; i< half_size ; i++){
         power *= 10;
     }
     
     long long h1 = x/power;
-    long long l1 = x%power;
+    long long l1= x%power;
     
     long long h2 = y/power;
     long long l2 = y%power;
     
-    long long z0 = karatsuba(l1,l2);
-    long long z1 = karatsuba (l1 + h1 , l2 + h2);
-    long long z2 = karatsuba(h1,h2);
+    long long s1= karatsuba(h1,h2);
+    long long s2 = karatsuba(l1,l2);
+    long long s3 = karatsuba(h1+l1,h2+l2);
     
-    
-    return z2 * power * power + (z1 - z2 - z0) * power + z0;
+    return s1*power*power + (s3-s2-s1)*power + s2;
 }
 
 
-
 int main(){
-    long long x = 12;
-    long long y = 12;
+    long long a = 112345;
+    long long b = 124521;
     
-    long long ans = karatsuba(x,y);
+    long long ans = karatsuba(a,b);
     cout << ans << endl;
+    
 }
