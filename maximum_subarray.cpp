d3999adb2572b64682bd4da7d3452a6141934b99
Ayush Raj <161280074+ascendantayush@users.noreply.github.com>
2025-03-10 01:14:35 +0530

    Update maximum_subarray.cpp
    new updated one

--- maximum_subarray.cpp
+++ maximum_subarray.cpp
@@ -1,89 +1,87 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <climits>
 using namespace std;
 
-struct SA{
+struct sub_array{
     int sum;
     int start;
     int end;
 };
 
 
-SA mid_sub_array(int arr[] , int low, int mid , int high){
-    int leftsum = INT_MIN;
-    int sum = 0;
-    int left_index = mid;
+sub_array crossum(int arr[],int l , int m , int r){
+    int left_sum = INT_MIN;
+    int left_index = m;
+    int temp_sum = 0;
     
-    for(int i = mid ; i >= low ; i--){
-        sum += arr[i];
-        if(sum > leftsum){
-            leftsum =sum;
+    for(int i = m ; i>= 0; i--){
+        temp_sum += arr[i];
+        if(temp_sum > left_sum){
+            left_sum = temp_sum;
             left_index = i;
         }
     }
     
     
-    sum = 0;
-    int right_index = mid + 1;
-    int rightsum = INT_MIN;
+    int right_sum = INT_MIN;
+    int right_index = m+1;
+    temp_sum = 0;
     
-    for(int i = mid + 1; i <= high ; i++){
-        sum += arr[i];
-        if(sum > rightsum){
-            rightsum = sum;
+    for(int i = m+1; i <= r ; i++){
+        temp_sum += arr[i];
+        if(temp_sum > right_sum){
+            right_sum = temp_sum;
             right_index = i;
         }
     }
     
+    sub_array result;
+    result.sum = left_sum + right_sum;
+    result.start = left_index;
+    result.end = right_index;
     
-    
-    SA answer;
-    answer.sum = leftsum + rightsum;
-    answer.start = left_index;
-    answer.end = right_index;
-    
-    return answer;
+    return result;
 }
 
-SA maxi(SA l, SA r, SA c){
-    if((l.sum > r.sum) && (l.sum > c.sum)) return l;
-    if((r.sum > l.sum) && (r.sum > c.sum)) return r;
-    return c;
+sub_array maxi(sub_array l,sub_array r,sub_array c){
+    if(l.sum > r.sum && l.sum > c.sum) return l;
+    else if(r.sum > l.sum && r.sum > c.sum) return r;
+    else  return c;
 }
 
 
 
-SA max_sub_array(int arr[] , int low , int high){
-    if(low == high){
-        SA answer;
-        answer.sum = arr[low];
-        answer.start = low;
-        answer.end = high;
-        
-        return answer;
+sub_array maximumSubArray(int arr[] , int l , int r){
+    if( l == r){
+        sub_array ans;
+        ans.sum = arr[l];
+        ans.start = l;
+        ans.end = l;
+    
+        return ans;
     }
     
-    int mid = (low + high) / 2;
+    int mid = (l+r)/2 ;
+    sub_array left = maximumSubArray(arr,l,mid);
+    sub_array right = maximumSubArray(arr,mid+1,r);
+    sub_array center = crossum(arr,l,mid,r);
     
-    SA left_subarray = max_sub_array(arr,low, mid);
-    SA right_subarray = max_sub_array(arr,mid+1,high);
-    SA mid_subarray = mid_sub_array(arr,low,mid,high);
+    //find the maximum
     
-    SA answer = maxi(left_subarray,right_subarray,mid_subarray);
+    sub_array answer = maxi(left,right,center);
     
     return answer;
 }
 
 
 int main(){
-    
-    int arr[6] = {-1,2,3,1,-2,3};
-    SA final_ans = max_sub_array(arr,0,5);
-    
-    cout << "maximum subarray sum is : " << final_ans.sum << endl;
-    cout << "and the maximum subarray is : " ;
-    for(int i = final_ans.start ; i <= final_ans.end ; i++){
+    int arr[7] = {-2,-3,2,4,6,-1,7};
+    sub_array res = maximumSubArray(arr,0,6);
+    cout << res.sum << endl;
+    for(int i = res.start ; i<= res.end ; i++){
         cout << arr[i] << " ";
     }
     
     cout << endl;
+    return 0;
 }
